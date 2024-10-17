2c8e15b99fa0e07aa98c041aa95e954306a2cb66
Shubham Rai <shubham.rai.iitg@gmail.com>
2024-10-17 17:49:57 +0530

    added test for handling istio header

---
+++ resolver/internal/hostManager/hostManager_test.go
@@ -0,0 +1,55 @@
+package hostManager
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/truefoundry/elasti/pkg/messages"
+	"go.uber.org/zap"
+)
+
+func TestGetHost(t *testing.T) {
+	logger, _ := zap.NewDevelopment()
+	hm := NewHostManager(logger, 10*time.Second, "X-Envoy-Decorator-Operation")
+
+	tests := []struct {
+		name          string
+		req           *http.Request
+		expectedHost  *messages.Host
+		expectedError bool
+	}{
+		{
+			name: "Host in header",
+			req: &http.Request{
+				Host: "example.com",
+				Header: http.Header{
+					"X-Envoy-Decorator-Operation": []string{"service.namespace.svc.cluster.local:8080/test/*"},
+				},
+			},
+			expectedHost: &messages.Host{
+				IncomingHost:   "service.namespace.svc.cluster.local:8080/test/*",
+				Namespace:      "namespace",
+				SourceService:  "service",
+				TargetService:  "elasti-service-pvt-9df6b026a8",
+				SourceHost:     "http://service.namespace.svc.cluster.local:8080",
+				TargetHost:     "http://elasti-service-pvt-9df6b026a8.namespace.svc.cluster.local:8080",
+				TrafficAllowed: true,
+			},
+			expectedError: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			host, err := hm.GetHost(tt.req)
+			if tt.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.expectedHost, host)
+		})
+	}
+}
