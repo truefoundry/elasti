39e85d760d6f2a54121dc30d3fdfbbacf4fc08a5
Shubham Rai <shubham.rai.iitg@gmail.com>
2025-05-30 13:29:45 +0530

    test fixes

--- operator/internal/controller/suite_test.go
+++ operator/internal/controller/suite_test.go
@@ -124,7 +124,7 @@ var _ = BeforeSuite(func() {
 		InformerStartLocks: sync.Map{},
 		ReconcileLocks:     sync.Map{},
 	}
-	Expect(controllerReconciler.SetupWithManager(mgr)).To(Succeed())
+	Expect(controllerReconciler.SetupWithManager(mgr, metav1.NamespaceAll)).To(Succeed())
 })
 
 var _ = AfterSuite(func() {
