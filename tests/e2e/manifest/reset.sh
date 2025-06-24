1bca2667fb7db74a8be9f312d53702280d384470
Raman Tehlan <ramantehlan@gmail.com>
2025-06-24 19:54:13 +0530

    e2e tests: all tests working

---
+++ tests/e2e/manifest/reset.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Apply ElastiService
+kubectl apply -f  $1/target-elastiservice.yaml -n target
+
+# Scale target-deployment back to 1
+kubectl scale deployment target-deployment -n target --replicas=1
+
+# Reset the service selector and port
+kubectl patch service target-deployment -n target --type=merge -p '{"spec":{"selector":{"app":"target-deployment"},"ports":[{"port":80,"targetPort":8080}]}}'
+
+# Scale elasti-resolver back to 1
+kubectl scale deployment elasti-resolver -n elasti --replicas=1
+
+# Wait for resources to be ready
+kubectl wait pods -l app=target-deployment -n target --for=condition=Ready --timeout=60s
+kubectl wait pods -l app=elasti-resolver -n elasti --for=condition=Ready --timeout=60s
+kubectl get elastiservice -n target target-elastiservice || exit 1
